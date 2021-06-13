289b4e42f8ef28c70704ec68e526fe7194031a37
bhavani <84915460+bhavani125@users.noreply.github.com>
2021-06-13 19:26:46 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,6 @@ package javapractice;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-	System.out.println("HelloWorld welcome to github");
-	}
+	System.out.println("HelloWorld welcome to github.com");
+		}
 }
